ec5924a6d205bc6cb537500facf1c6db51713f59
HedTB <hugobasq@gmail.com>
2021-12-09 16:54:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -189,6 +189,6 @@ def redirect_unauthorized(e):
 
 def start_app(bot):
     # from waitress import serve
-    # serve(app, host="localhost", port=5000
+    # serve(app, host="0.0.0.0", port=5000
     app.bot = bot
-    app.run(debug=True, host="localhost", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
