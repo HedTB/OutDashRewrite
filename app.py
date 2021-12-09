74ed624f7ea3383e7ae2ed209c4002cdabf664bc
HedTB <hugobasq@gmail.com>
2021-12-09 20:00:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -63,9 +63,9 @@ def index():
     
     guilds = get_guilds_with_permission()
     
-    print(hasattr(app, "bot"))
-    print(app.bot.get_guild(836495137651294258))
-    print(app.bot.users)
+    # print(hasattr(app, "bot"))
+    # print(app.bot.get_guild(836495137651294258))
+    # print(app.bot.users)
 
     return render_template('servers.html', render_avatar=avatar, render_username=f'{username}#{usertag}', render_guilds=guilds)
 
