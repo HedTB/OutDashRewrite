ddf6be7042e2497e8441df41b3168b9c6e01ff80
HedTB <hugobasq@gmail.com>
2021-12-09 15:22:28 +0100

    e

--- cogs/bot_settings/app_functions.py
+++ cogs/bot_settings/app_functions.py
@@ -13,7 +13,7 @@ from discord.commands.commands import Option
 from discord.errors import Forbidden, HTTPException
 from discord.ext.commands import errors
 from pymongo import MongoClient
-from main import bot
+#  from main import bot
 
 # FILES
 import bot_info
@@ -23,13 +23,13 @@ class AppFunctions(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         
-    def check_for_bot_in_server(guild_id: int):
-        guild = bot.get_guild(859482895009579039)
-        print(guild)
-        if guild:
-            return guild
-        else:
-            return None
+    # def check_for_bot_in_server(guild_id: int):
+    #     guild = bot.get_guild(859482895009579039)
+    #     print(guild)
+    #     if guild:
+    #         return guild
+    #     else:
+    #         return None
 
 
 def setup(bot):
