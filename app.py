578e85a4518348c2048a6265496e8aa9d0b5c7a4
HedTB <hugobasq@gmail.com>
2021-12-10 21:14:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from discord.ext import ipc
 app = Quart(__name__)
 ipc_client = ipc.Client(secret_key=b"%\xe0'\x01\xdeH\x8e\x85m|\xb3\xffCN\xc9g", port=5000)
 
-ipc_client.init_sock()
+asyncio.run(ipc_client.init_sock())
 load_dotenv()
 
 app.secret_key = b"%\xe0'\x01\xdeH\x8e\x85m|\xb3\xffCN\xc9g"
