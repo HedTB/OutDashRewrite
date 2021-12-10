b10e84615b8a080c5d29e3e2edae25de4417c9ad
HedTB <hugobasq@gmail.com>
2021-12-10 10:24:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from discord.ext import ipc
 ## -- VARIABLES -- ##
 
 app = Quart(__name__)
-ipc_client = ipc.Client(secret_key="HedTBIsHandsome", host="0.0.0.0", port=23448)
+ipc_client = ipc.Client(secret_key="HedTBIsHandsome", host="0.0.0.0", port=8080)
 load_dotenv()
 
 app.secret_key = b"%\xe0'\x01\xdeH\x8e\x85m|\xb3\xffCN\xc9g"
