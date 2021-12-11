4441c7e1fdca964ee90c861aac5a3dd0b590ec6a
HedTB <hugobasq@gmail.com>
2021-12-11 19:44:18 +0100

    fuck asyncio

--- ipc/discord/ext/ipc/server.py
+++ ipc/discord/ext/ipc/server.py
@@ -258,8 +258,6 @@ class Server:
             self._multicast_server = aiohttp.web.Application()
             self._multicast_server.router.add_route("GET", "/", self.handle_multicast)
 
-            task = self.loop.create_task(self.__start(self._multicast_server, self.multicast_port))
-            self.loop.run_until_complete(task)
+            await self.__start(self._multicast_server, self.multicast_port)
 
-        task = self.loop.create_task(self.__start(self._server, self.port))
-        self.loop.run_until_complete(task)
+        await self.__start(self._server, self.port)
