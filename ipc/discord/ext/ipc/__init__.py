3de0aa885544a4cf8c8870b54702ea6870a88027
HedTB <hugobasq@gmail.com>
2021-12-11 18:43:07 +0100

    Update __init__.py

--- ipc/discord/ext/ipc/__init__.py
+++ ipc/discord/ext/ipc/__init__.py
@@ -4,7 +4,6 @@ from ipc.discord.ext.ipc.client import Client
 from ipc.discord.ext.ipc.server import Server
 from ipc.discord.ext.ipc.errors import *
 
-
 _VersionInfo = collections.namedtuple("_VersionInfo", "major minor micro release serial")
 
 version = "2.1.1"
