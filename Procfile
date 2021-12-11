c745dcc282999f84512d587fe6d24036b3da0709
HedTB <hugobasq@gmail.com>
2021-12-11 12:01:40 +0100

    e

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 heroku ps:scale worker=1
 worker: python main.py
-web: hypercorn -b 0.0.0.0:8080 app:app
+web: hypercorn -b 0.0.0.0:$PORT app:app
--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from main import export_bot
 ## -- VARIABLES -- ##
 
 app = Quart(__name__)
-ipc_client = ipc.Client(secret_key=b"%\xe0'\x01\xdeH\x8e\x85m|\xb3\xffCN\xc9g")
+# ipc_client = ipc.Client(secret_key=b"%\xe0'\x01\xdeH\x8e\x85m|\xb3\xffCN\xc9g")
 
 load_dotenv()
 
