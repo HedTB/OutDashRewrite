e95f41aa69894a947f7b97ee8c22afb946c363f4
HedTB <hugobasq@gmail.com>
2022-03-01 15:52:28 +0100

    Update stats_tracking.py

--- commands/loops/stats_tracking.py
+++ commands/loops/stats_tracking.py
@@ -16,12 +16,15 @@ class StatsTracking(commands.Cog):
     async def custom_graph_1(self):
         return 1 + 2 + 3
     
-    @tasks.loop(seconds=90)
+    @tasks.loop(seconds=300)
     async def statcord_loop(self):
-        print(requests.post(
+        response = requests.post(
             url = "https://statcord.com/bot/836494578135072778/clearcache",
             headers = {"cookie": "_gid=GA1.2.306018653.1646068095; connect.sid=s%3A7g9VQBYdQgvgyKSSsVq2uIZY7e2D8Toa.lYSN0MMxOLrCffnM%2Foym4bfL0DLogLfPv3uGnjxAIow; _ga=GA1.2.572428963.1646068094; _gat_gtag_UA_170926897_1=1; _ga_JT5NSL8623=GS1.1.1646072266.3.1.1646073702.0"}
-        ).status_code)
+        )
+        
+        if response.status_code != 200:
+            print(response.json())
 
 
 def setup(bot):
